c22efd4fd0c9eceef478a85f5836ee7ff8a606ab
Ankur <sharma@Ankur-MacBook-Pro.local>
2026-08-31 22:24:14 +0530

    Allow Google Maps in CSP frame-src

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const securityHeaders = [
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: https: blob:",
-      "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://checkout.razorpay.com",
+      "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://checkout.razorpay.com https://maps.google.com https://www.google.com",
       "connect-src 'self' https://*.supabase.co https://api.stripe.com https://resend.com",
     ].join("; "),
   },
